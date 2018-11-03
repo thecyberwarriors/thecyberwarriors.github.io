127bf50876847b8ce7bd6e8202e929e4af558587
trojan2Golf <adam.rosenbaum.engineer@gmail.com>
2018-11-03 14:32:04 -0700

    auto-generated workflow

---
+++ development/js/script.js
@@ -0,0 +1,54 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 70)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 100
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+
+  // override 'enter' keypress for input
+  $('input').keydown(function(event) {
+    if (event.keyCode == 13) {
+      event.preventDefault();
+    }
+  });
+
+  $('input').click(function(event) {
+    window.open("https://goo.gl/forms/mdgMjnMdwDpdf0Y63", "_blank");
+  });
+
+})(jQuery); // End of use strict
